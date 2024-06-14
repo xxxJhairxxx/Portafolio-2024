d827d8970ca0886a343fd4b5fd9f02440c52ada9
jhair <jinfanzonquispe30@gmail.com>
2024-06-14 06:20:44 -0500

    add sharp

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// astro.config.mjs
+
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
+export default defineConfig({
+  integrations: [
+    tailwind(),
+  ],
+});
